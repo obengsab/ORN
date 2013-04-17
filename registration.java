f1b4e8ef35ce982c662947b442c9b01713791622
obengsab <sobeng@mail.umw.edu>
2013-04-17 20:06:15 -0300

    Update registration.java

--- registration.java
+++ registration.java
@@ -1,13 +1,40 @@
+
+/**
+ * 
+ * Description: The registration class creates a new registration class object
+ * that adds a new user to the database. 
+ * 
+ * Authors: Daniela Cardona, Morgan Brown,  Sonia Obeng
+ * Language: Java
+ * Date: April 3, 2013
+ * 
+ **/
+
+
 public class registration {
 
-DBConnect connect = new DBConnect( );
+ 
+DBConnect connect = new DBConnect( ); // new DBconnect object
+
 
 registration( ){
+  //default constructor
 }
 
+
+/**
+ * This method inserts new user's username and password in the database.
+ * @param user holds the string of user's created username
+ * @param pass holds the string of users's created password
+ * @return true user is now registered
+ * */
+
 public boolean register(String user, String pass){
+  
 connect.insertUser(user,pass);
+
 return true;
+
 }
 
 
