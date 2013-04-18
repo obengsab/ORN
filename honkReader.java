9a5b127e7c169affcf38099424e58b0610e39b53
obengsab <sobeng@mail.umw.edu>
2013-04-17 21:32:27 -0300

    Update honkReader.java

--- honkReader.java
+++ honkReader.java
@@ -1,3 +1,16 @@
+/**
+ * 
+ * Description: The honkReader class has all methods related to a posts- when 
+ * needed to be posted, searched, and displayed.  
+ * 
+ * Authors: Daniela Cardona, Morgan Brown,  Sonia Obeng
+ * Language: Java
+ * Date: April 3, 2013
+ * 
+ **/
+
+
+
 import java.util.*;
 
 public class honkReader {
@@ -5,7 +18,17 @@ public class honkReader {
 DBConnect connect = new DBConnect( );
 
 honkReader( ){
+    //default constructor
 }
+
+
+/**
+ *This method checks if user chooses to make post private or public
+*@param user holds the string of user's username
+*@param honk holds the string of user's honk post
+* @return false if user is not in database
+* @return false returns false if user does not choose public or private
+* */
 public boolean post(String user, String honk, String setting){
 
 if(!connect.getUser(user)){
@@ -22,6 +45,11 @@ return true;
 }
 }
 
+/**
+ *This method gets all posts posted by a user
+*@param username holds the string of user's username
+* @return honk returns all "honks"/ posts posted by a user. 
+* */
 public LinkedList<String> getAllPosts(String username){
 LinkedList<String> honkList = connect.getPublicPost( );
 if(username == "null"){
@@ -46,6 +74,12 @@ while(privPostsIterator.hasNext( )){
 return honkList;
 }
 
+
+/**
+ *This method gets all users current user is subscribed to 
+*@param username holds the string of user's username
+* @return honk returns all "honks"/ posts posted by a subscriber.
+* */
 public LinkedList<String> getSubscribers(String username){    
 LinkedList<String> honkList = connect.getSubscribe(username);
 if(username == "null"){
@@ -64,6 +98,16 @@ honkList.add(subPrivPostsIt.next( ));
 }
 return honkList;
 }
+
+/**
+ *This method...
+*@param username 
+* @param keyword 
+* @return error 
+* @return finalReturn 
+* */
+
+//Guys, I cannot understand what this methos is doing.
 public LinkedList<String> search(String username, String keyword){
 LinkedList<String> error = new LinkedList<>( );
 error.add("The search term was not found.");
